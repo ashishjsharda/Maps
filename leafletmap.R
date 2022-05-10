593bc0a9fe55094e62d4bc812b3b22911d9ebfe1
Ashish S <ashishjsharda@gmail.com>
2022-05-10 04:39:08 -0500

    Create leafletmap.R

---
+++ leafletmap.R
@@ -0,0 +1,9 @@
+library(leaflet)
+library(maps)
+
+maps::state.carto.center
+mapState<-map('state',fill = TRUE,plot = FALSE)
+head(mapState$names)
+head(mapState$x)
+head(mapState$y)
+leaflet(data = mapState) %>% addTiles()
